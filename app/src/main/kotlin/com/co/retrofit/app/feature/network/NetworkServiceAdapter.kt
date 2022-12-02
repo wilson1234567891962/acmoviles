eda0afe0833b91ff59b30be55ec2f24e2341a2a9
Alejandro Gonzalez <wilson1234567891962@hotmail.com>
2022-12-02 14:37:14 -0500

    Change the enpoint

--- app/src/main/kotlin/com/co/retrofit/app/feature/network/NetworkServiceAdapter.kt
+++ app/src/main/kotlin/com/co/retrofit/app/feature/network/NetworkServiceAdapter.kt
@@ -22,7 +22,7 @@ import org.json.JSONObject
 
 class NetworkServiceAdapter constructor(context: Context) {
     companion object{
-        const val BASE_URL= "https://vinilosg1.herokuapp.com/"
+        const val BASE_URL= "http://34.173.213.18:3000/"
         var instance: NetworkServiceAdapter? = null
         fun getInstance(context: Context) =
             instance ?: synchronized(this) {
